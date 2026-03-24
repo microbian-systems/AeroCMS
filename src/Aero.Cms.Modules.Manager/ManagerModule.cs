104d2b68ec1792279f9f77fdedd1e7b88b14c263
bbqchickenrobot <bbqchickenrobot@gmail.com>
2026-03-24 13:42:15 -0500

    manager module

---
+++ src/Aero.Cms.Modules.Manager/ManagerModule.cs
@@ -0,0 +1,28 @@
+﻿using Aero.Cms.Core;
+using Aero.Cms.Web.Core.Modules;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Aero.Cms.Modules.Manager;
+
+public class ManagerModule : AeroModuleBase
+{
+    public override string Name { get; } = nameof(ManagerModule);
+    public override string Version { get; } = AeroVersion.Version;
+    public override string Author { get; } = AeroConstants.Author;
+    public override IReadOnlyList<string> Dependencies { get; } = [];
+    public override IReadOnlyList<string> Category { get; } = [];
+    public override IReadOnlyList<string> Tags { get; } = [];
+
+    public override void ConfigureServices(IServiceCollection services, IConfiguration? config = null, IHostEnvironment? env = null)
+    {
+        base.ConfigureServices(services, config, env);
+    }
+
+    public override Task RunAsync(IEndpointRouteBuilder builder)
+    {
+        return base.RunAsync(builder);
+    }
+}
